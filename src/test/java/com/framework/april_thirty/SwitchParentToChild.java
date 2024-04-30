ca7e68a438a8c73ab67f7069d42a3acf6c7c60df
Sandeepvirk0590 <sandeep.cor14@gmail.com>
2024-04-30 16:30:30 -0400

    Sixth part 2

--- src/test/java/com/framework/april_thirty/SwitchParentToChild.java
+++ src/test/java/com/framework/april_thirty/SwitchParentToChild.java
@@ -42,6 +42,7 @@ public class SwitchParentToChild {
 
 		WebElement text = wd.findElement(By.id("sampleHeading"));
 		System.out.println(text.getText() + "Hlooooooooooo");
+		System.out.println(text.getText() + "Hlooooooooooo");
 		System.out.println(text.getAttribute(parentWindow));
 
 
